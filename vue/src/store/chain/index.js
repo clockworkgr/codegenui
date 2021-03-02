cc22ffb2d0ce8203bc4e176ebfeed605641c1208
Clockwork <alex@clockwork.gr>
2021-03-02 17:59:49 +0200

    chain index typo

--- vue/src/store/chain/index.js
+++ vue/src/store/chain/index.js
@@ -1,3 +1,3 @@
 import CosmosBankV1beta1 from './cosmos/cosmos-sdk/cosmos.bank.v1beta1'
-import ClockworkgrCodegenUiBlog from './clockworkgr/codegenui/clockworkgr.codegenui.blog'
-export default { CosmosBankV1beta1, ClockworkgrCodegenUiBlog }
+import ClockworkgrCodegenuiBlog from './clockworkgr/codegenui/clockworkgr.codegenui.blog'
+export default { CosmosBankV1beta1, ClockworkgrCodegenuiBlog }
